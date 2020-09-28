98f2393c75963d28dd64256f2eeba5011ae051e0
Christian <kpnguyen1013@gmail.com>
2020-09-28 02:15:50 -0500

    Add content to about section

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,8 +1,13 @@
-const navLinks = document.querySelector('.nav-links');
+const navLinksList = document.querySelector('.nav-links');
 const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelectorAll('.nav-link');
 
 function slideNav() {
-    navLinks.classList.toggle('nav-links-active');
+    navLinksList.classList.toggle('nav-links-active');
+    console.log('worked')
 }
 
 hamburger.addEventListener('click', slideNav);
+navLinks.forEach(link => {
+    link.addEventListener('click', slideNav)
+})
